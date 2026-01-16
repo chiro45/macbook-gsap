c860f1a9b78567e84fa435c6761910e693bf3eba
Luciano Chiroli <luciano.chiroli45@gmail.com>
2026-01-15 23:45:12 -0300

    add productViewer to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Hero } from "./components/Hero";
 import { NavBar } from "./components/NavBar";
+import { ProducViewer } from "./components/ProducViewer";
 
 export const App = () => {
   return (
     <div>
       <main>
-        <NavBar/>
-        <Hero/>
+        <NavBar />
+        <Hero />
+        <ProducViewer />
       </main>
     </div>
   );
